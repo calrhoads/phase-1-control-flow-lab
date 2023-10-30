05006bb9386fca626b4bdaa0a03e5f66713beda6
CalRhoads <calrrhoads@gmail.com>
2023-10-30 15:54:10 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+
+  if (distance<= 200) {
+  return "This one is on me!"
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+  if (distance>= 400 && distance<=2000) {
+  return "That will be twenty bucks."}
+
+  if (distance>=2000 && distance<=2500) {
+    return "I will gladly take your thirty bucks."
+  }
+  
+  if (distance > 2500) {
+    return "No can do."
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go."
+
+}
+
+function switchOnCharmFromTip(thanks){
+  switch (thanks) {
+     case "generous":
+      thanks = "Thank you so much."
+      break;
+    case "not as generous":
+      thanks = "Thank you."
+      break;
+    case "thanks for everything":
+      thanks = "Bye."
+      break;
+  }
+  return thanks
 }
